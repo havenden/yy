ffe34b7f94b5f536f7c05afbaf6104c3c2c3d214
aiden <1098003693@qq.com>
2020-09-18 07:57:51 +0800

    'add-add-no'

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -22,6 +22,7 @@ class ApiController extends Controller
             $table='swts_'.$hid;
             if (Schema::hasTable($table)){
                 $swtCount=DB::table($table)->where([['start_time','>=',$start], ['start_time','<=',$end]])->count();
+                $swtAddNoCount=DB::table($table)->where([['start_time','>=',$start], ['start_time','<=',$end],['member_type'],'广告否词'])->count();//广告否词
                 $swtEffectiveCount=DB::table($table)->where([
                     ['start_time','>=',$start], ['start_time','<=',$end],['is_effective',1]
                 ])->count();
@@ -30,11 +31,14 @@ class ApiController extends Controller
                 ])->groupBy('author')->pluck('c','author')->toArray();
                 $body['all']=$swtCount;
                 $body['effective']=$swtEffectiveCount;
+                $body['addNo']=$swtAddNoCount;
                 foreach ($swtsArray as $me=>$num){
                     $me_all=$num;
                     $me_effective=DB::table($table)->where([['author','=',$me],['start_time','>=',$start], ['start_time','<=',$end],['is_effective',1]])->count();
+                    $me_add_no=DB::table($table)->where([['author','=',$me],['start_time','>=',$start], ['start_time','<=',$end],['member_type','广告否词']])->count();
                     $body['authors'][$me]['all']=$me_all;//数量
                     $body['authors'][$me]['effective']=$me_effective;
+                    $body['authors'][$me]['add_no']=$me_add_no;
                     $body['authors'][$me]['percent']=$swtCount>0?sprintf('%.2f',($me_all/$swtCount)*100).'%':0;
                     $body['authors'][$me]['effective_percent']=$swtEffectiveCount>0?sprintf('%.2f',($me_effective/$swtEffectiveCount)*100).'%':0;
                 }
